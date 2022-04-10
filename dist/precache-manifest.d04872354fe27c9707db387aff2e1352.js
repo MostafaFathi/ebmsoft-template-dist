bebd4732f84eb397232641df5f7e2aaeb5365fc8
Mostafa Hilles <hilles@etree.com.sa>
2022-04-11 01:54:58 +0300

    first build

---
+++ dist/precache-manifest.d04872354fe27c9707db387aff2e1352.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182ee99ba984b37c87e1",
+    "url": "/css/app.6f4d7bf4.css"
+  },
+  {
+    "revision": "79086740c3d7a35defe7",
+    "url": "/css/chunk-vendors.68964720.css"
+  },
+  {
+    "revision": "fdff7c19febf396d6c5e635f6b7c3889",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "56fcf4e0cec35ab5f1ffd9b642c5389a",
+    "url": "/img/Polaris-UI-Kit.zip"
+  },
+  {
+    "revision": "57ae8ef740936715078c9d5dd13d78c1",
+    "url": "/img/coming-soon.png"
+  },
+  {
+    "revision": "a31fe70564060689e74d280f5b488aaa",
+    "url": "/img/logo-image.png"
+  },
+  {
+    "revision": "5f38f9c1666e4ddf900658f931b9ceea",
+    "url": "/img/logo-image.zip"
+  },
+  {
+    "revision": "9cbdec3a80503bf6f098dac5b9ae570c",
+    "url": "/img/logo-image960x640.png"
+  },
+  {
+    "revision": "e0056ee2ccdff3545f54c5c042de4df3",
+    "url": "/img/logo-white.png"
+  },
+  {
+    "revision": "aefb966a868508b0c6f0b6e2fe82d514",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "d6eec034e36eecfacd1fd9a0699ac469",
+    "url": "/img/logo.zip"
+  },
+  {
+    "revision": "d7d0dc9492f5d69370886569c65bed3d",
+    "url": "/img/team/ebrahim-maher.jpg"
+  },
+  {
+    "revision": "ed50c6825c0bf8f6b37752daa7c12610",
+    "url": "/img/vector1.png"
+  },
+  {
+    "revision": "0b41f7b3275158be7ba39012c4d37916",
+    "url": "/img/vector2.png"
+  },
+  {
+    "revision": "06e3ee6c2c0344f56d476933e7508a6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "182ee99ba984b37c87e1",
+    "url": "/js/app.d045fa24.js"
+  },
+  {
+    "revision": "79086740c3d7a35defe7",
+    "url": "/js/chunk-vendors.c62befef.js"
+  },
+  {
+    "revision": "8f283c41a61b6440d100382ddc94d5de",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
